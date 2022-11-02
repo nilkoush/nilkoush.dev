01c5e3a9e2a0f52a8fb458bdca3aba99a7c12921
nikolas <nikolasstanga@gmail.com>
2022-11-02 16:52:22 +0100

    added redirects

--- next.config.js
+++ next.config.js
@@ -3,6 +3,30 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	trailingSlash: true,
+	async redirects() {
+		return [
+			{
+				source: '/instagram',
+				destination: 'https://www.instagram.com/nilkoush',
+				permanent: true,
+			},
+			{
+				source: '/dribbble',
+				destination: 'https://www.dribbble.com/nilkoush',
+				permanent: true,
+			},
+			{
+				source: '/github',
+				destination: 'https://www.github.com/nilkoush',
+				permanent: true,
+			},
+			{
+				source: '/twitter',
+				destination: 'https://www.twitter.com/nilkoush',
+				permanent: true,
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
