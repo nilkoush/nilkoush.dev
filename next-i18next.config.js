43e808c7a34afbd49d574a7333fe8ad50b758254
nikolas <nikolasstanga@gmail.com>
2023-02-04 15:59:52 +0100

    updated config to let it work with vercel

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,4 +1,4 @@
-// @ts-check
+const path = require('path');
 
 /**
  * @type {import('next-i18next').UserConfig}
@@ -7,5 +7,6 @@ module.exports = {
 	i18n: {
 		locales: ['en', 'cs'],
 		defaultLocale: 'en',
+		localePath: path.resolve('./public/locales'),
 	},
 };
