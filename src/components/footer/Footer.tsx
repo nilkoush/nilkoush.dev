4370341108d9365713d85df906245ab5c5d6b4ce
nikolas <nikolasstanga@gmail.com>
2023-02-16 16:03:59 +0100

    changed some description

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,64 +4,64 @@ import { SiDribbble, SiGithub, SiInstagram, SiTwitter } from 'react-icons/si';
 interface FooterProps {}
 
 const Footer: FC<FooterProps> = () => {
-	return (
-		<>
-			<footer className="flex w-full flex-col items-center justify-center py-4">
-				<h2 className="text-2xl font-semibold text-black/50 dark:text-white/50">
-					Nikolas Štanga
-				</h2>
-				<p className="text-base text-black/60 dark:text-white/30">
-					web / app designer and developer
-				</p>
-				<ul className="flex">
-					<li>
-						<a
-							className="block p-2"
-							href="https://www.instagram.com/nilkoush"
-							target="_blank"
-							rel="noreferrer"
-							aria-label="Link for Instagram"
-						>
-							<SiInstagram className="h-5 w-5 fill-black/50 dark:fill-white/50" />
-						</a>
-					</li>
-					<li>
-						<a
-							className="block p-2"
-							href="https://www.dribbble.com/nilkoush"
-							target="_blank"
-							rel="noreferrer"
-							aria-label="Link for Dribbble"
-						>
-							<SiDribbble className="h-5 w-5 fill-black/50 dark:fill-white/50" />
-						</a>
-					</li>
-					<li>
-						<a
-							className="block p-2"
-							href="https://www.github.com/nilkoush"
-							target="_blank"
-							rel="noreferrer"
-							aria-label="Link for GitHub"
-						>
-							<SiGithub className="h-5 w-5 fill-black/50 dark:fill-white/50" />
-						</a>
-					</li>
-					<li>
-						<a
-							className="block p-2"
-							href="https://www.twitter.com/nilkoush"
-							target="_blank"
-							rel="noreferrer"
-							aria-label="Link for Twitter"
-						>
-							<SiTwitter className="h-5 w-5 fill-black/50 dark:fill-white/50" />
-						</a>
-					</li>
-				</ul>
-			</footer>
-		</>
-	);
+    return (
+        <>
+            <footer className="flex w-full flex-col items-center justify-center py-4">
+                <h2 className="text-2xl font-semibold text-black/50 dark:text-white/50">
+                    Nikolas Štanga
+                </h2>
+                <p className="text-base text-black/60 dark:text-white/30">
+                    software engineer
+                </p>
+                <ul className="flex">
+                    <li>
+                        <a
+                            className="block p-2"
+                            href="https://www.instagram.com/nilkoush"
+                            target="_blank"
+                            rel="noreferrer"
+                            aria-label="Link for Instagram"
+                        >
+                            <SiInstagram className="h-5 w-5 fill-black/50 dark:fill-white/50" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className="block p-2"
+                            href="https://www.dribbble.com/nilkoush"
+                            target="_blank"
+                            rel="noreferrer"
+                            aria-label="Link for Dribbble"
+                        >
+                            <SiDribbble className="h-5 w-5 fill-black/50 dark:fill-white/50" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className="block p-2"
+                            href="https://www.github.com/nilkoush"
+                            target="_blank"
+                            rel="noreferrer"
+                            aria-label="Link for GitHub"
+                        >
+                            <SiGithub className="h-5 w-5 fill-black/50 dark:fill-white/50" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className="block p-2"
+                            href="https://www.twitter.com/nilkoush"
+                            target="_blank"
+                            rel="noreferrer"
+                            aria-label="Link for Twitter"
+                        >
+                            <SiTwitter className="h-5 w-5 fill-black/50 dark:fill-white/50" />
+                        </a>
+                    </li>
+                </ul>
+            </footer>
+        </>
+    );
 };
 
 export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,102 +9,102 @@ import { GetServerSideProps } from 'next';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import {
-	SiGit,
-	SiGithub,
-	SiJavascript,
-	SiMysql,
-	SiNextdotjs,
-	SiNodedotjs,
-	SiNpm,
-	SiPrisma,
-	SiReact,
-	SiShopify,
-	SiTailwindcss,
-	SiTypescript,
-	SiVisualstudiocode,
-	SiWordpress,
+    SiGit,
+    SiGithub,
+    SiJavascript,
+    SiMysql,
+    SiNextdotjs,
+    SiNodedotjs,
+    SiNpm,
+    SiPrisma,
+    SiReact,
+    SiShopify,
+    SiTailwindcss,
+    SiTypescript,
+    SiVisualstudiocode,
+    SiWordpress,
 } from 'react-icons/si';
 
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
-	return {
-		props: {
-			...(await serverSideTranslations(locale as string, [
-				'common',
-				'home',
-			])),
-		},
-	};
+    return {
+        props: {
+            ...(await serverSideTranslations(locale as string, [
+                'common',
+                'home',
+            ])),
+        },
+    };
 };
 
 const HomePage: NextPageWithLayout = () => {
-	const { t } = useTranslation(['common', 'home']);
+    const { t } = useTranslation(['common', 'home']);
 
-	return (
-		<>
-			<motion.main
-				className="container"
-				initial={{ opacity: 0, scale: 0.95 }}
-				animate={{ opacity: 1, scale: 1 }}
-				exit={{ opacity: 0, scale: 0.95 }}
-				transition={{ ease: 'easeOut', duration: 0.15 }}
-			>
-				<Section
-					title={t('home:about_me.title')}
-					subtitle={t('common:pages.about_me')}
-				>
-					<p
-						className="prose dark:prose-invert"
-						dangerouslySetInnerHTML={{
-							__html: t('home:about_me.body', {
-								interpolation: { escapeValue: false },
-							}),
-						}}
-					/>
-					<h3 className="text-2xl font-bold">
-						{t('home:what_i_do.title')}
-					</h3>
-					<p
-						className="prose dark:prose-invert"
-						dangerouslySetInnerHTML={{
-							__html: t('home:what_i_do.body', {
-								interpolation: { escapeValue: false },
-							}),
-						}}
-					/>
-					<ul className="mb-12 flex flex-wrap items-center justify-center rounded-md border border-gray-800 bg-white/10 p-1 dark:bg-black/10">
-						<TechItem icon={SiTypescript} name="TypeScript" />
-						<TechItem icon={SiVisualstudiocode} name="VSCode" />
-						<TechItem icon={SiJavascript} name="Javascript" />
-						<TechItem icon={SiGithub} name="GitHub" />
-						<TechItem icon={SiGit} name="Git" />
-						<TechItem icon={SiReact} name="ReactJS" />
-						<TechItem icon={SiNextdotjs} name="NextJS" />
-						<TechItem icon={SiTailwindcss} name="TailwindCSS" />
-						<TechItem icon={SiWordpress} name="Wordpress" />
-						<TechItem icon={SiPrisma} name="Prisma" />
-						<TechItem icon={SiMysql} name="MySQL" />
-						<TechItem icon={SiNodedotjs} name="NodeJS" />
-						<TechItem icon={SiShopify} name="Shopify" />
-						<TechItem icon={SiNpm} name="NPM" />
-					</ul>
-				</Section>
-			</motion.main>
-		</>
-	);
+    return (
+        <>
+            <motion.main
+                className="container"
+                initial={{ opacity: 0, scale: 0.95 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0.95 }}
+                transition={{ ease: 'easeOut', duration: 0.15 }}
+            >
+                <Section
+                    title={t('home:about_me.title')}
+                    subtitle={t('common:pages.about_me')}
+                >
+                    <p
+                        className="prose dark:prose-invert"
+                        dangerouslySetInnerHTML={{
+                            __html: t('home:about_me.body', {
+                                interpolation: { escapeValue: false },
+                            }),
+                        }}
+                    />
+                    <h3 className="text-2xl font-bold">
+                        {t('home:what_i_do.title')}
+                    </h3>
+                    <p
+                        className="prose dark:prose-invert"
+                        dangerouslySetInnerHTML={{
+                            __html: t('home:what_i_do.body', {
+                                interpolation: { escapeValue: false },
+                            }),
+                        }}
+                    />
+                    <ul className="mb-12 flex flex-wrap items-center justify-center rounded-md border border-gray-800 bg-white/10 p-1 dark:bg-black/10">
+                        <TechItem icon={SiTypescript} name="TypeScript" />
+                        <TechItem icon={SiVisualstudiocode} name="VSCode" />
+                        <TechItem icon={SiJavascript} name="Javascript" />
+                        <TechItem icon={SiGithub} name="GitHub" />
+                        <TechItem icon={SiGit} name="Git" />
+                        <TechItem icon={SiReact} name="ReactJS" />
+                        <TechItem icon={SiNextdotjs} name="NextJS" />
+                        <TechItem icon={SiTailwindcss} name="TailwindCSS" />
+                        <TechItem icon={SiWordpress} name="Wordpress" />
+                        <TechItem icon={SiPrisma} name="Prisma" />
+                        <TechItem icon={SiMysql} name="MySQL" />
+                        <TechItem icon={SiNodedotjs} name="NodeJS" />
+                        <TechItem icon={SiShopify} name="Shopify" />
+                        <TechItem icon={SiNpm} name="NPM" />
+                    </ul>
+                </Section>
+            </motion.main>
+        </>
+    );
 };
 
 HomePage.getLayout = (page) => {
-	return (
-		<Layout
-			title="Nikolas Štanga"
-			description="I'm a self-taught young freelance web / app designer and also developer from Czech Republic. I've been doing stuff around web for nearly 3 years now."
-			url="https://www.nilkoush.dev/"
-		>
-			<Navbar />
-			{page}
-			<Footer />
-		</Layout>
-	);
+    return (
+        <Layout
+            title="Nikolas Štanga"
+            description="I'm a self-taught young software engineer from Czech Republic. I've been doing stuff around it for nearly 3 years now."
+            url="https://www.nilkoush.dev/"
+        >
+            <Navbar />
+            {page}
+            <Footer />
+        </Layout>
+    );
 };
 
 export default HomePage;
