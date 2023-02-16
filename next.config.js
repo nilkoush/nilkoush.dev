eb559b9642dacd972eece418c99afc50a22903a1
nikolas <nikolasstanga@gmail.com>
2023-02-16 16:22:35 +0100

    changed redirects

--- next.config.js
+++ next.config.js
@@ -2,39 +2,34 @@ const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	swcMinify: true,
-	trailingSlash: true,
-	i18n,
-	async redirects() {
-		return [
-			{
-				source: '/instagram',
-				destination: 'https://www.instagram.com/nilkoush',
-				permanent: true,
-			},
-			{
-				source: '/dribbble',
-				destination: 'https://www.dribbble.com/nilkoush',
-				permanent: true,
-			},
-			{
-				source: '/github',
-				destination: 'https://www.github.com/nilkoush',
-				permanent: true,
-			},
-			{
-				source: '/twitter',
-				destination: 'https://www.twitter.com/nilkoush',
-				permanent: true,
-			},
-			{
-				source: '/discord',
-				destination: 'https://discord.gg/Xwjymdu2vW',
-				permanent: true,
-			},
-		];
-	},
+    reactStrictMode: true,
+    swcMinify: true,
+    trailingSlash: true,
+    i18n,
+    async redirects() {
+        return [
+            {
+                source: '/github',
+                destination: 'https://www.github.com/nilkoush',
+                permanent: true,
+            },
+            {
+                source: '/twitter',
+                destination: 'https://www.twitter.com/nilkoush',
+                permanent: true,
+            },
+            {
+                source: '/linkedin',
+                destination: 'https://www.linkedin.com/in/nikolasstanga',
+                permanent: true,
+            },
+            {
+                source: '/discord',
+                destination: 'https://discord.gg/Xwjymdu2vW',
+                permanent: true,
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
