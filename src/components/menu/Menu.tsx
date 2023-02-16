333b8151b385761ebef44caba8de6c87b8083102
nikolas <nikolasstanga@gmail.com>
2023-02-16 15:52:28 +0100

    changed social media

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -3,68 +3,63 @@ import MenuItem from '@components/menu/MenuItem';
 import { motion } from 'framer-motion';
 import { useTranslation } from 'next-i18next';
 import { FC } from 'react';
-import { SiDribbble, SiGithub, SiInstagram, SiTwitter } from 'react-icons/si';
+import { SiGithub, SiLinkedin, SiTwitter } from 'react-icons/si';
 
 interface MenuProps {
-	closeMenu: () => void;
+    closeMenu: () => void;
 }
 
 const Menu: FC<MenuProps> = ({ closeMenu }) => {
-	const { t } = useTranslation('common');
+    const { t } = useTranslation('common');
 
-	return (
-		<>
-			<motion.div
-				initial={{ opacity: 0 }}
-				animate={{ opacity: 1 }}
-				exit={{ opacity: 0 }}
-				transition={{ duration: 0.1, ease: 'easeInOut' }}
-				className="fixed z-10 block h-screen w-full overflow-hidden bg-black/10 backdrop-blur-md lg:hidden"
-				onClick={closeMenu}
-			></motion.div>
-			<motion.nav
-				initial={{ opacity: 0 }}
-				animate={{ opacity: 1 }}
-				exit={{ opacity: 0 }}
-				transition={{ duration: 0.1, ease: 'easeInOut' }}
-				className="z-20 flex flex-col gap-4 bg-gradient-to-bl from-white to-[#E5E6E4] pb-4 dark:from-black dark:to-[#0d131f] lg:hidden"
-			>
-				<ul className="flex flex-col items-center justify-center">
-					<MenuItem href="/" closeMenu={closeMenu}>
-						{t('pages.about_me')}
-					</MenuItem>
-					<MenuItem href="/portfolio" closeMenu={closeMenu}>
-						{t('pages.portfolio')}
-					</MenuItem>
-					<MenuItem href="/contact" closeMenu={closeMenu}>
-						{t('pages.contact')}
-					</MenuItem>
-				</ul>
-				<ul className="flex justify-center">
-					<MenuIcon
-						icon={<SiInstagram className="h-6 w-6" />}
-						title="Instagram"
-						href="https://www.instagram.com/nilkoush"
-					/>
-					<MenuIcon
-						icon={<SiDribbble className="h-6 w-6" />}
-						title="Dribbble"
-						href="https://www.dribbble.com/nilkoush"
-					/>
-					<MenuIcon
-						icon={<SiGithub className="h-6 w-6" />}
-						title="GitHub"
-						href="https://www.github.com/nilkoush"
-					/>
-					<MenuIcon
-						icon={<SiTwitter className="h-6 w-6" />}
-						title="Twitter"
-						href="https://www.twitter.com/nilkoush"
-					/>
-				</ul>
-			</motion.nav>
-		</>
-	);
+    return (
+        <>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.1, ease: 'easeInOut' }}
+                className="fixed z-10 block h-screen w-full overflow-hidden bg-black/10 backdrop-blur-md lg:hidden"
+                onClick={closeMenu}
+            ></motion.div>
+            <motion.nav
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.1, ease: 'easeInOut' }}
+                className="z-20 flex flex-col gap-4 bg-gradient-to-bl from-white to-[#E5E6E4] pb-4 dark:from-black dark:to-[#0d131f] lg:hidden"
+            >
+                <ul className="flex flex-col items-center justify-center">
+                    <MenuItem href="/" closeMenu={closeMenu}>
+                        {t('pages.about_me')}
+                    </MenuItem>
+                    <MenuItem href="/portfolio" closeMenu={closeMenu}>
+                        {t('pages.portfolio')}
+                    </MenuItem>
+                    <MenuItem href="/contact" closeMenu={closeMenu}>
+                        {t('pages.contact')}
+                    </MenuItem>
+                </ul>
+                <ul className="flex justify-center">
+                    <MenuIcon
+                        icon={<SiGithub className="h-6 w-6" />}
+                        title="GitHub"
+                        href="https://www.github.com/nilkoush"
+                    />
+                    <MenuIcon
+                        icon={<SiTwitter className="h-6 w-6" />}
+                        title="Twitter"
+                        href="https://www.twitter.com/nilkoush"
+                    />
+                    <MenuIcon
+                        icon={<SiLinkedin className="h-6 w-6" />}
+                        title="LinkedIn"
+                        href="https://www.linkedin.com/in/nikolasstanga/"
+                    />
+                </ul>
+            </motion.nav>
+        </>
+    );
 };
 
 export default Menu;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -8,84 +8,79 @@ import { AnimatePresence } from 'framer-motion';
 import { useTranslation } from 'next-i18next';
 import Link from 'next/link';
 import { FC, useState } from 'react';
-import { SiDribbble, SiGithub, SiInstagram, SiTwitter } from 'react-icons/si';
+import { SiGithub, SiLinkedin, SiTwitter } from 'react-icons/si';
 
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = () => {
-	const { t } = useTranslation('common');
-	const [menu, setMenu] = useState(false);
+    const { t } = useTranslation('common');
+    const [menu, setMenu] = useState(false);
 
-	const closeMenu = () => {
-		setMenu(false);
-	};
+    const closeMenu = () => {
+        setMenu(false);
+    };
 
-	const toggleMenu = () => {
-		setMenu(!menu);
-	};
+    const toggleMenu = () => {
+        setMenu(!menu);
+    };
 
-	return (
-		<>
-			<div className="fixed top-0 left-0 z-10 flex w-full flex-col">
-				<header className="z-30 flex items-center justify-center border-b border-gray-800/50 bg-[#F0F0F0]/60 px-4 py-2 backdrop-blur-lg dark:bg-[#12181d]/60">
-					<nav className="container flex items-center justify-between">
-						<h1 className="text-2xl font-bold">
-							<Link href="/">
-								<a>nilkoush.dev</a>
-							</Link>
-						</h1>
-						<ul className="hidden items-center justify-center gap-2 lg:flex">
-							<NavbarLink href="/">
-								{t('pages.about_me')}
-							</NavbarLink>
-							<NavbarLink href="/portfolio">
-								{t('pages.portfolio')}
-							</NavbarLink>
-							<NavbarLink href="/contact">
-								{t('pages.contact')}
-							</NavbarLink>
-						</ul>
-						<div className="flex items-center justify-center gap-4">
-							<ul className="hidden items-center justify-center lg:flex">
-								<NavbarIcon
-									icon={<SiInstagram className="h-6 w-6" />}
-									title="Instagram"
-									href="https://www.instagram.com/nilkoush"
-								/>
-								<NavbarIcon
-									icon={<SiDribbble className="h-6 w-6" />}
-									title="Dribbble"
-									href="https://www.dribbble.com/nilkoush"
-								/>
-								<NavbarIcon
-									icon={<SiGithub className="h-6 w-6" />}
-									title="GitHub"
-									href="https://www.github.com/nilkoush"
-								/>
-								<NavbarIcon
-									icon={<SiTwitter className="h-6 w-6" />}
-									title="Twitter"
-									href="https://www.twitter.com/nilkoush"
-								/>
-							</ul>
-							<hr className="hidden h-8 w-[2px] border-none bg-slate-900/50 dark:bg-white/50 lg:block" />
-							<div className="flex items-center justify-center">
-								<NavbarTheme />
-								<NavbarLanguage />
-								<NavbarMenu
-									isOpened={menu}
-									toggleMenu={toggleMenu}
-								/>
-							</div>
-						</div>
-					</nav>
-				</header>
-				<AnimatePresence mode="wait">
-					{menu && <Menu closeMenu={closeMenu} />}
-				</AnimatePresence>
-			</div>
-		</>
-	);
+    return (
+        <>
+            <div className="fixed top-0 left-0 z-10 flex w-full flex-col">
+                <header className="z-30 flex items-center justify-center border-b border-gray-800/50 bg-[#F0F0F0]/60 px-4 py-2 backdrop-blur-lg dark:bg-[#12181d]/60">
+                    <nav className="container flex items-center justify-between">
+                        <h1 className="text-2xl font-bold">
+                            <Link href="/">
+                                <a>nilkoush.dev</a>
+                            </Link>
+                        </h1>
+                        <ul className="hidden items-center justify-center gap-2 lg:flex">
+                            <NavbarLink href="/">
+                                {t('pages.about_me')}
+                            </NavbarLink>
+                            <NavbarLink href="/portfolio">
+                                {t('pages.portfolio')}
+                            </NavbarLink>
+                            <NavbarLink href="/contact">
+                                {t('pages.contact')}
+                            </NavbarLink>
+                        </ul>
+                        <div className="flex items-center justify-center gap-4">
+                            <ul className="hidden items-center justify-center lg:flex">
+                                <NavbarIcon
+                                    icon={<SiGithub className="h-6 w-6" />}
+                                    title="GitHub"
+                                    href="https://www.github.com/nilkoush"
+                                />
+                                <NavbarIcon
+                                    icon={<SiTwitter className="h-6 w-6" />}
+                                    title="Twitter"
+                                    href="https://www.twitter.com/nilkoush"
+                                />
+                                <NavbarIcon
+                                    icon={<SiLinkedin className="h-6 w-6" />}
+                                    title="LinkedIn"
+                                    href="https://www.linkedin.com/in/nikolasstanga/"
+                                />
+                            </ul>
+                            <hr className="hidden h-8 w-[2px] border-none bg-slate-900/50 dark:bg-white/50 lg:block" />
+                            <div className="flex items-center justify-center">
+                                <NavbarTheme />
+                                <NavbarLanguage />
+                                <NavbarMenu
+                                    isOpened={menu}
+                                    toggleMenu={toggleMenu}
+                                />
+                            </div>
+                        </div>
+                    </nav>
+                </header>
+                <AnimatePresence mode="wait">
+                    {menu && <Menu closeMenu={closeMenu} />}
+                </AnimatePresence>
+            </div>
+        </>
+    );
 };
 
 export default Navbar;
